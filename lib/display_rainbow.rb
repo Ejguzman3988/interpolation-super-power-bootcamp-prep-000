84370c127a6fdec2971baf9bc21506d74f631943
Eriberto Guzman <eriberto176@gmail.com>
2020-04-26 11:14:21 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,7 +2,7 @@
 
 def display_rainbow(colors)
   
-  puts "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet\n"
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, V: #{colors[5]}"
 
 end
 
